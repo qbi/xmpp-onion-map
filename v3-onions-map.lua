75e4306f4f137568b8337c6f501560e669cae708
qbi <github@kubieziel.de>
2019-10-03 14:20:08 +0200

    Add nr18.space

--- v3-onions-map.lua
+++ v3-onions-map.lua
@@ -1,3 +1,4 @@
 onions_map = {
 	["krautspace.de"] = "jeirlvruhz22jqduzixi6li4xyoweytqglwjons4mbuif76fgslg5uad.onion";
+	["nr18.space"] = "szd7r26dbcrrrn4jthercrdypxfdmzzrysusyjohn4mpv2zbwcgmeqqd.onion";
 };
