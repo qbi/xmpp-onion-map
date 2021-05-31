e4cdf79ecc2620fe552abbf2f10d3ba2b59faf57
Jens Kubieziel <github@kubieziel.de>
2021-05-31 14:13:24 +0000

    Add v3 onion for jabber.otr.im

--- v3-onions-map.lua
+++ v3-onions-map.lua
@@ -1,4 +1,5 @@
 onions_map = {
+	["jabber.otr.im"] = "ynnuxkbbiy5gicdydekpihmpbqd4frruax2mqhpc35xqjxp5ayvrjuqd.onion";
 	["krautspace.de"] = "jeirlvruhz22jqduzixi6li4xyoweytqglwjons4mbuif76fgslg5uad.onion";
 	["jabber.nr18.space"] = "szd7r26dbcrrrn4jthercrdypxfdmzzrysusyjohn4mpv2zbwcgmeqqd.onion";
 };
