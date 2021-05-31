87472f44b380d6ac97eeb8d10f87c2d094074327
Jens Kubieziel <github@kubieziel.de>
2021-05-31 14:27:28 +0000

    add v3 onion of wiuwiu.de

--- v3-onions-map.lua
+++ v3-onions-map.lua
@@ -3,4 +3,4 @@ onions_map = {
 	["jabber.otr.im"] = "ynnuxkbbiy5gicdydekpihmpbqd4frruax2mqhpc35xqjxp5ayvrjuqd.onion";
 	["krautspace.de"] = "jeirlvruhz22jqduzixi6li4xyoweytqglwjons4mbuif76fgslg5uad.onion";
 	["jabber.nr18.space"] = "szd7r26dbcrrrn4jthercrdypxfdmzzrysusyjohn4mpv2zbwcgmeqqd.onion";
-};
+	["wiuwiu.de"] = "qawb5xl3mxiixobjsw2d45dffngyyacp4yd3wjpmhdrazwvt4ytxvayd.onion";
