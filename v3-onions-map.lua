f87b794a86494bb88b0994783ffc3e038f9d1186
Martin <spam-github@mdosch.de>
2024-02-19 20:31:19 +0100

    Update v3-onions-map.lua
    Add diebesban.de and mdosch.de

--- v3-onions-map.lua
+++ v3-onions-map.lua
@@ -1,5 +1,6 @@
 onions_map = {
 	["anrc.mooo.com"] = "6w5iasklrbr2kw53zqrsjktgjapvjebxodoki3gjnmvb4dvcbmz7n3qd.onion";
+	["diebesban.de"] = "bztoavotvspxwehf52gw7xrljvj7rjucpwdoixoaoxgpwqkoex27jnid.onion";
 	["dismail.de"] = "4colmnerbjz3xtsjmqogehtpbt5upjzef57huilibbq3wfgpsylub7yd.onion";
 	["jabber.cat"] = "x76it4hax7s6s6j64ta6iiolar6o6hwlwoeyoqmkjcdgjza6gqqj5gyd.onion";
 	["jabber.de"] = "uoj2xiqxk25p36wbpufiyuhluvxakhpqum7frembhoiuq7a5735ay3qd.onion";
@@ -8,6 +9,7 @@ onions_map = {
 	["jabber.systemli.org"] = "razpihro3mgydaiykvxwa44l57opvktqeqfrsg3vvwtmvr2srbkcihyd.onion";
 	["krautspace.de"] = "jeirlvruhz22jqduzixi6li4xyoweytqglwjons4mbuif76fgslg5uad.onion";
 	["jabber.nr18.space"] = "szd7r26dbcrrrn4jthercrdypxfdmzzrysusyjohn4mpv2zbwcgmeqqd.onion";
+	["mdosch.de"] = "bztoavotvspxwehf52gw7xrljvj7rjucpwdoixoaoxgpwqkoex27jnid.onion";
 	["talk36.net"] = "yxkc2uu3rlwzzhxf2thtnzd7obsdd76vtv7n34zwald76g5ogbvjbbqd.onion";
 	["wiuwiu.de"] = "qawb5xl3mxiixobjsw2d45dffngyyacp4yd3wjpmhdrazwvt4ytxvayd.onion";
 	["xmpp.is"] = "6voaf7iamjpufgwoulypzwwecsm2nu7j5jpgadav2rfqixmpl4d65kid.onion";
